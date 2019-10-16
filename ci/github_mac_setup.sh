da5f8d377b5c295223df2cf681102e84fd73ed03
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2019-10-16 16:38:22 +0900

    testing

--- ci/github_mac_setup.sh
+++ ci/github_mac_setup.sh
@@ -3,13 +3,13 @@
 set -xeu
 set -o pipefail
 
+export PATH="${PATH-}:$HOME/bin/"
+export LIBRARY_PATH="${LIBRARY_PATH-}:/usr/local/lib/"
+export PKG_CONFIG_PATH="/usr/local/opt/sqlite/lib/pkgconfig"
+
 brew install sqlite3
 
 mkdir -p $HOME/Dependencies/
 wget -P $HOME/Dependencies/ https://homebrew.bintray.com/bottles/libsodium-1.0.18.high_sierra.bottle.tar.gz
 tar -C /usr/local/Cellar/ -xf $HOME/Dependencies/libsodium-1.0.18.high_sierra.bottle.tar.gz
 brew link libsodium
-
-export PATH="${PATH-}:$HOME/bin/"
-export LIBRARY_PATH="${LIBRARY_PATH-}:/usr/local/lib/"
-export PKG_CONFIG_PATH="/usr/local/opt/sqlite/lib/pkgconfig"
